17b6cf4afd19421e80dfb1f44d7a3e2d9520c198
PHUNG <phung.duy@newunion.sg>
2019-01-30 16:25:49 +0700

    Split routes out into a separate controller

--- Sources/App/Controllers/AcronymsController.swift
+++ Sources/App/Controllers/AcronymsController.swift
@@ -2,12 +2,15 @@ import Vapor
 import Fluent
 struct AcronymsController: RouteCollection {
     
-    let acronymsRoutes = router.grouped("api", "acronyms")
+    
+    
     func boot(router: Router) throws {
         //router.get("api", "acronyms", use: getAllHandler)
+        let acronymsRoutes = router.grouped("api", "acronyms")
         acronymsRoutes.get(use: getAllHandler)
         // 1
-        acronymsRoutes.post(use: createHandler)
+        //acronymsRoutes.post(use: createHandler)
+        acronymsRoutes.post(Acronym.self, use: createHandler)
         // 2
         acronymsRoutes.get(Acronym.parameter, use: getHandler)
         // 3
@@ -25,4 +28,73 @@ struct AcronymsController: RouteCollection {
     func getAllHandler(_ req: Request) throws -> Future<[Acronym]> {
         return Acronym.query(on: req).all()
     }
+    
+    //lay content request decode sau do map. va save
+    func createHandler(_ req: Request) throws -> Future<Acronym> {
+        return try req
+            .content
+            .decode(Acronym.self)
+            .flatMap(to: Acronym.self) { acronym in
+                return acronym.save(on: req)
+        }
+        
+    }
+    //truyen model luon va save khi co ket qua!
+    func createHandler(_ req: Request,acronym: Acronym) throws -> Future<Acronym> {
+        return acronym.save(on: req)
+    }
+    
+    
+    
+    
+    func getHandler(_ req: Request) throws -> Future<Acronym> {
+        return try req.parameters.next(Acronym.self)
+    }
+    func updateHandler(_ req: Request) throws -> Future<Acronym> {
+        return try flatMap(
+            to: Acronym.self,
+            req.parameters.next(Acronym.self),
+            req.content.decode(Acronym.self)
+        ) { acronym, updatedAcronym in
+            acronym.short = updatedAcronym.short
+            acronym.long = updatedAcronym.long
+            return acronym.save(on: req)
+        }
+        
+    }
+    func deleteHandler(_ req: Request)
+        throws -> Future<HTTPStatus> {
+            return try req
+                .parameters
+                .next(Acronym.self)
+                .delete(on: req)
+                .transform(to: HTTPStatus.noContent)
+    }
+    func searchHandler(_ req: Request) throws -> Future<[Acronym]> {
+        guard let searchTerm = req
+            .query[String.self, at: "term"] else {
+                throw Abort(.badRequest)
+        }
+        return Acronym.query(on: req).group(.or) { or in
+            or.filter(\.short == searchTerm)
+            or.filter(\.long == searchTerm)
+            }.all()
+        
+    }
+    
+    func getFirstHandler(_ req: Request) throws -> Future<Acronym> {
+        return Acronym.query(on: req)
+            .first()
+            .map(to: Acronym.self) { acronym in
+                guard let acronym = acronym else {
+                    throw Abort(.notFound)
+                }
+                return acronym
+        }
+    }
+    func sortedHandler(_ req: Request) throws -> Future<[Acronym]> {
+        return Acronym.query(on: req).sort(\.short, .ascending).all()
+    }
+
+    
 }
